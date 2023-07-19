3ff782a3740fbce4d9534cf45fa135d2544bc36d
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-19 10:47:46 -0400

    update

--- myapp/components/Donut.js
+++ myapp/components/Donut.js
@@ -3,9 +3,19 @@ import React from 'react'
 const Donut = () => {
   return (
 
-      <div className='card w-70 h-80 mt-8 bg-base-100 shadow-xl'>
+      <div className='border=0 w-50 mt-8 bg-base-100 shadow-xl border-white-100'>
        <figure><img src="https://images.pexels.com/photos/6142740/pexels-photo-6142740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="donut" /></figure>
-
+       <div className="flex flex-col gap-4 p-3">
+    <h2 className="card-title">
+      Shoes!
+      <div className="badge badge-secondary">NEW</div>
+    </h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <div className="badge badge-outline">Fashion</div> 
+      <div className="badge badge-outline">Products</div>
+    </div>
+  </div>
       </div>
   )
 }
